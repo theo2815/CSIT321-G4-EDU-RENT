a28167375feeb92f940b48d9e2c0be78b96957bf
theo2815 <theocedricchan28@gmail.com>
2025-12-22 02:17:26 +0800

    feat: move all the API's keys into .env

--- edurentfrontend/src/services/apiService.js
+++ edurentfrontend/src/services/apiService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { supabase } from '../supabaseClient';
 
 // Backend URL
-const API_URL = 'http://localhost:8080/api/v1';
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Create an Axios instance to handle all API requests
 const apiClient = axios.create({
--- edurentfrontend/src/supabaseClient.js
+++ edurentfrontend/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://faeumfwnvnndehpdznch.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhZXVtZndudm5uZGVocGR6bmNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyMjgzNjMsImV4cCI6MjA3NjgwNDM2M30.-pgpkITg1CchJe-frVynXhAvHyb3mNsfRL0C_uVupgs'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
